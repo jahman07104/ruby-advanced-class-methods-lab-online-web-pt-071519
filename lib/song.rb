d9a9093abaeb3d55609fc9be73b524f99c4bf4b2
Patrick Harrison <jahman@bellatlantic.net>
2019-08-12 15:29:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,16 +19,25 @@ class Song
     song
   end
     def self.create_by_name(name)
+      
     song = self.new 
+    
     song.name = name
+    
     @@all << song
     song
   end
    def self.find_by_name(name)
+     
     @@all.find{|x| x.name == name}
   end
   
   def self.find_or_create_by_name(name)
+    
      self.find_by_name(name) || self.create_by_name(name)
-  end   
+  end 
+  def self.alphabetical()
+    
+    @@all.sort_by{|x| x.name}
+  end
 end
